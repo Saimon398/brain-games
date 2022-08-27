4cf0bc616c67dab27fe160d88b935509ad88bd8b
alex_p <aleksborislavskij@gmail.com>
2022-08-27 21:12:52 +0300

    feat: added brain-even game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import sayGreetings from '../src/cli.js';
 
-// Привествуем пользователя
 sayGreetings();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,12 @@
-// Импортируем библиотеку в данный модуль
 import readlineSync from 'readline-sync';
 
-// Пишем функцию, которая запрашивает имя
+/**
+ * @description greetings
+ * @returns {String} player's name
+ */
 const sayGreetings = () => {
   const playerName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name ? ');
   console.log(`Hello, ${playerName}!`);
+  return playerName;
 };
 export default sayGreetings;
