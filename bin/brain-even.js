c8e10b90128c3e3efd6a90b7a989dd57d4929188
alex_p <aleksborislavskij@gmail.com>
2022-09-02 17:24:25 +0300

    refactor: change path to even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import startGame from '../src/index.js';
-import { sayIsEven, condition } from '../games/even/even.js';
+import { sayIsEven, condition } from '../games/even.js';
 import generateRandomNumber from '../src/random.js';
 
 startGame(generateRandomNumber, sayIsEven, condition);
