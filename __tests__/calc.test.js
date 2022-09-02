33a44f6b70b2da1c3e2bcd0e585caae96ab87745
alex_p <aleksborislavskij@gmail.com>
2022-09-02 17:51:10 +0300

    style: turned-odd "no-def" rule in eslint

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -3,12 +3,11 @@ import { calcExpression } from '../games/calc.js';
 describe.each([
   { expression: '10 + 20', expected: 30 },
   { expression: '10 - 45', expected: -35 },
-  { expression: '10 - 3',  expected: 7 },
-  { expression: '5 * 12',  expected: 60 },
-  { expression: '0 * 23',  expected: 0 }
+  { expression: '10 - 3', expected: 7 },
+  { expression: '5 * 12', expected: 60 },
+  { expression: '0 * 23', expected: 0 },
 ])('.add($expression)', ({ expression, expected }) => {
   test('Overall perfomance', () => {
     expect(calcExpression(expression)).toEqual(expected);
   });
 });
-
