d86f5786e73dc68762d569a7ffc711f977ac8913
alex_p <aleksborislavskij@gmail.com>
2022-09-02 23:14:15 +0300

    refactor: changed condition in prime.js if it is 1

--- __tests__/prime.test.js
+++ __tests__/prime.test.js
@@ -5,7 +5,7 @@ describe.each([
   { value: 7, expected: 'yes' },
   { value: 10, expected: 'no' },
   { value: 0, expected: 'yes' },
-  { value: 1, expected: 'yes' },
+  { value: 1, expected: 'no' },
   { value: -10, expected: 'no' },
   { value: -7, expected: 'yes' },
 ])('.add($value', ({ value, expected }) => {
